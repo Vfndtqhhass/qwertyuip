b3a6b8803460a4543d7f2bfffc039b165f5dface
Vfndtqhhass <huraneemar55@gmail.com>
2024-11-25 16:41:47 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    // Обработка получения сообщения
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // Рассылка сообщения всем клиентам
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
